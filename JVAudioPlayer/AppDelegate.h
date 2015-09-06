f0fd3f5ce32156eb83cb724ba88848dd63bc7b7e
Developer <developer@192-168-1-112.tpgi.com.au>
2015-09-06 17:48:04 +1000

    second commit

---
+++ JVAudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JVAudioPlayer
+//
+//  Created by Developer on 6/09/2015.
+//  Copyright (c) 2015 Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JVAudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JVAudioPlayer
+//
+//  Created by Developer on 6/09/2015.
+//  Copyright (c) 2015 Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
